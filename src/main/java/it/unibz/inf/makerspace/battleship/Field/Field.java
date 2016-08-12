0c5e7667e6c742bd6ad9c41d278a405a004430c2
Luca Sabiucciu <Luca.Sabiucciu@stud-inf.unibz.it>
2016-08-12 23:03:40 +0200

    Added attack function

--- src/main/java/it/unibz/inf/makerspace/battleship/Field/Field.java
+++ src/main/java/it/unibz/inf/makerspace/battleship/Field/Field.java
@@ -10,15 +10,11 @@ class Field
 	
 	public void initialize()
 	{
-		shipsA = new int[] {2,2,4,4,4,4};
-		shipsB = new int[] {2,2,4,4,4,4};
-		
 		fieldA = new int[8][8];
 		fieldB = new int[8][8]; 
 		
 		Arrays.fill(fieldA, 0);
 		Arrays.fill(fieldB, 0);
-		
 	}
 
 	public void setShip(String field, int xStart, int yStart, int xFinish, int yFinish)
@@ -42,5 +38,46 @@ class Field
 				for(int i=xStart; i<=xFinish; i++)
 					fieldA[yStart][i] = xStart*yStart*xFinish*yFinish;
 		}
+	}
+	
+	public int attack(String player, int x, int y)
+	{
+		boolean killedx = true, killedy = true; 
+		int ship;
+		if(player.equals("A"))
+		{
+			if(fieldA[x][y]==0)
+				return -1;
+			
+			ship = fieldA[x][y];
+
+			for(int i=0;i<fieldA.length;i++)
+				if(fieldA[i][y] == ship)
+					killedx = false;
+			
+			for(int i=0;i<fieldA[x].length;i++)
+				if(fieldA[x][i] == ship)
+					killedy = false;
+		}
+		else
+		{
+			if(fieldB[x][y]==0)
+				return -1;
+			
+			ship = fieldB[x][y];
+			
+			for(int i=0;i<fieldB.length;i++)
+				if(fieldB[i][y] == ship)
+					killedx = false;
+			
+			for(int i=0;i<fieldB[x].length;i++)
+				if(fieldB[x][i] == ship)
+					killedy = false;
+		}
+		
+		if(killedx && killedy)
+			return 1;
+		else
+			return 0;
 	}
 }
